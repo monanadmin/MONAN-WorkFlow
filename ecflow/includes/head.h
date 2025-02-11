e739e7d324590b497523c8b2c923458f050a0cd2
carlosrenatosouza2 <carlos.renato.souza@gmail.com>
2025-02-11 17:04:33 -0300

    direct on develop: main modification is chenge de main directory to STAB and DEV. old suite MONAN_STAB and MONAN_TESTCASES are now installed into /mnt/beegfs/monan/STAB/MONAN-WorkFlow directory - new scripts start_DEV.ksh and stop_DEV.ksh where created to make a parallel DEV server with a different port_ECF number. new script clone_MONAN-scripts_CDCT.bash where created to clone the scripts_CD-CT into the right place to be called from ecf scripts correctly; VERSION ECF tag label where also added to be shown in the ecf window i think at this point, a version tag should be created of this repo.

--- ecflow/includes/head.h
+++ ecflow/includes/head.h
@@ -47,7 +47,7 @@ trap '{ echo "Killed by a signal"; ERROR ; }' 1 2 3 4 5 6 7 8 10 12 13 15
 #  Exports de datas/dirs importantes
 #
 export FAMILY1=%FAMILY1%
-
+export DIRSUITE=/mnt/beegfs/monan/STAB/MONAN-WorkFlow/ecflow
 
 
 
